2faf5bdf5aee1fba90498845d8e0db5525240b65
altamashGit <altamashalam102@gmail.com>
2026-05-17 19:01:44 +0530

    terraform: Backend s3 backet

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,14 @@
 provider "aws" {
     alias = "mumbai"
-    region = var.aws_region
-  
+    region = var.aws_region 
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket"
+    key            = "dev/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
 }
