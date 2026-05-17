a002039cc08f036fe3f13761c890a34c003daf40
altamashGit <altamashalam102@gmail.com>
2026-05-17 14:19:16 +0530

    ADDED- IAC USER DATA

--- terrafrom/varable.tf
+++ terrafrom/varable.tf
@@ -13,14 +13,14 @@ variable "instace_type" {
 variable "ami_id" {
     description = "The AMI ID to use for the instance"
     type        = string
-    default     = "ami-05d2d839d4f73aafb"
+    default     = "ami-09ed39e30153c3bf9"
   
 }
 
 variable "key_path" {
     description = "The path to the SSH key pair for accessing the instance"
     type        = string
-    default     = "c:/users/altam/.ssh/id_ed25519.pub"  
+    default     = "/c/Users/altam/github-actions-kubernetes-masterclass/hackathon.pub"  
 }
 
 variable "server_count" {
