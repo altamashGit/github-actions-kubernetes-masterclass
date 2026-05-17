989e54ac830cccb36f83a5c4ab827137cd8f5804
altamashGit <altamashalam102@gmail.com>
2026-05-17 19:33:26 +0530

    provider edited

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,13 +2,3 @@ provider "aws" {
     alias = "mumbai"
     region = var.aws_region 
 }
-
-terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state-bucket"
-    key            = "dev/terraform.tfstate"
-    region         = "us-west-2"
-    use_lockfile   = false
-    encrypt        = true
-  }
-}
