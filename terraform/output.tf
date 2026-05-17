acefaf02ae60b7077a3b13b4bb74954e58650d8d
altamashGit <altamashalam102@gmail.com>
2026-05-17 15:21:25 +0530

    Fix: IAC output.tf corrected

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,11 +1,11 @@
-output "instance_public_ips" {
+output "terraform_ec2_instance" {
   value = aws_instance.terraform_ec2_instance.public_ip
 }
 
 output "instance_public_dns" {
-  value = aws_instance.my_instance.public_dns
+  value = aws_instance.terraform_ec2_instance.public_dns
 }
 
 output "instance_id" {
-  value = aws_instance.my_instance.id
+  value = aws_instance.terraform_ec2_instance.id
   }
